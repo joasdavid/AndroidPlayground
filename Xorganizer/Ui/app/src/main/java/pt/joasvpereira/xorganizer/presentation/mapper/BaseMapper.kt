048fc60fcd01a05417608794f9e8503c163b4731
joas.pereira <joasdavid@gmail.com>
2022-06-27 00:59:24 +0100

    xorganizer/logic create mappers for Box/SingleBox and StoredItem/SingleItem, also create extended fun to map list of items.

--- Xorganizer/Ui/app/src/main/java/pt/joasvpereira/xorganizer/presentation/mapper/BaseMapper.kt
+++ Xorganizer/Ui/app/src/main/java/pt/joasvpereira/xorganizer/presentation/mapper/BaseMapper.kt
@@ -2,5 +2,14 @@ package pt.joasvpereira.xorganizer.presentation.mapper
 
 interface BaseMapper<PRESENTATION, DOMAIN> {
     fun mapToPresentation(from: DOMAIN): PRESENTATION
+
+    //fun mapToPresentation(from: List<DOMAIN>): List<PRESENTATION>
     fun mapToDomain(from: PRESENTATION): DOMAIN
+    //fun mapToDomain(from: List<PRESENTATION>): List<DOMAIN>
 }
+
+fun <PRESENTATION, DOMAIN> BaseMapper<PRESENTATION, DOMAIN>.mapToPresentationList(from: List<DOMAIN>): List<PRESENTATION> =
+    from.map { mapToPresentation(it) }
+
+fun <PRESENTATION, DOMAIN> BaseMapper<PRESENTATION, DOMAIN>.mapToDomainList(from: List<PRESENTATION>): List<DOMAIN> =
+    from.map { mapToDomain(it) }
