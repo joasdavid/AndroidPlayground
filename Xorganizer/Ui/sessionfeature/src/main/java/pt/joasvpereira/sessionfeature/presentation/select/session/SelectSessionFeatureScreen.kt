f32d6282f0592823f9c28b28a90ddac2189b0b70
joas.pereira <joasdavid@gmail.com>
2023-02-16 15:25:23 +0000

    mix improvements

--- Xorganizer/Ui/sessionfeature/src/main/java/pt/joasvpereira/sessionfeature/presentation/select/session/SelectSessionFeatureScreen.kt
+++ Xorganizer/Ui/sessionfeature/src/main/java/pt/joasvpereira/sessionfeature/presentation/select/session/SelectSessionFeatureScreen.kt
@@ -3,7 +3,7 @@ package pt.joasvpereira.sessionfeature.presentation.select.session
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
