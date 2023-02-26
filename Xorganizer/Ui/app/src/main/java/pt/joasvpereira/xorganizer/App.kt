090b87a6b01a4913423adb2ed115648c9b2081be
joasdavid <joasdavid@gmail.com>
2023-02-26 16:00:41 +0000

    #11 test hook nr 2.

--- Xorganizer/Ui/app/src/main/java/pt/joasvpereira/xorganizer/App.kt
+++ Xorganizer/Ui/app/src/main/java/pt/joasvpereira/xorganizer/App.kt
@@ -10,9 +10,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import pt.joasvpereira.core.settings.di.coreDependencies
 import pt.joasvpereira.sessionfeature.di.sessionFeatureModule
-import pt.joasvpereira.xorganizer.di.repository
-import pt.joasvpereira.xorganizer.di.usecases
-import pt.joasvpereira.xorganizer.di.viewModelModule
+import pt.joasvpereira.xorganizer.di.*
 
 class App : Application() {
     override fun onCreate() {
