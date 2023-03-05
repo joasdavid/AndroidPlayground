b6f4830afaa50e81a9cddf622408d7a57e98f20e
joasdavid <joasdavid@gmail.com>
2023-03-05 11:57:57 +0000

    add detekt to the project. (#27)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ buildscript {
     dependencies {
         classpath(BuildPlugins.android)
         classpath(BuildPlugins.kotlin)
+        classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -22,6 +23,7 @@ apply(from = "buildSrc/git-hooks.gradle")
 
 subprojects {
     apply(from = "../plugins/Ktlint.gradle")
+    apply(from = "../plugins/detekt.gradle")
 }
 
 tasks.register("clean", Delete::class) {
