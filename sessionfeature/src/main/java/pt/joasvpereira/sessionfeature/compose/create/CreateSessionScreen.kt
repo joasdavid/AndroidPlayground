e1e2c4ca8b71598e078d5477622b542d377b9327
joasdavid <joasdavid@gmail.com>
2023-03-07 17:53:39 +0000

    #22 adapt CreateSessionScreen to fit smaller devices.

--- sessionfeature/src/main/java/pt/joasvpereira/sessionfeature/compose/create/CreateSessionScreen.kt
+++ sessionfeature/src/main/java/pt/joasvpereira/sessionfeature/compose/create/CreateSessionScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.joasvpereira.lib.compose.spacer.SimpleSpace
+import com.joasvpereira.lib.compose.spacer.VerticalSpace
 import pt.joasvpereira.coreui.scaffold.AppScaffold
 import pt.joasvpereira.coreui.scaffold.ToolBarConfig
 import pt.joasvpereira.coreui.selector.UploadImagePlaceHolder
@@ -47,6 +48,7 @@ internal fun CreateSessionScreen(
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 SimpleSpace(size = 60.dp)
+
                 UploadImagePlaceHolder(
                     emptyText = sessionName,
                     bitmap = bitmap,
@@ -54,41 +56,68 @@ internal fun CreateSessionScreen(
                     onClearImageClick = onClearImageClick,
                 )
                 SimpleSpace(size = 45.dp)
+
                 AppTextField(
                     value = sessionName,
                     onValueChange = onSessionNameChange,
                     placeholder = "Session Name",
                 )
+
+                VerticalSpace(height = 45.dp)
+
+                ButtonsColumn(
+                    modifier = Modifier
+                        .fill75PercentWidth()
+                        .padding(bottom = 40.dp),
+                    onButtonCreateClick = onButtonCreateClick,
+                    isButtonEnabled = isButtonEnabled,
+                    isOnEditMode = isOnEditMode,
+                    onButtonDeleteClick = onButtonDeleteClick,
+                )
             }
-            Column(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(bottom = 40.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
+        }
+    }
+}
+
+@Suppress("MagicNumber")
+fun Modifier.fill75PercentWidth() = then(Modifier.fillMaxWidth(.75f))
+
+@Composable
+private fun ButtonsColumn(
+    modifier: Modifier = Modifier,
+    horizontalAlignment: Alignment.Horizontal = Alignment.CenterHorizontally,
+    onButtonCreateClick: () -> Unit,
+    isButtonEnabled: Boolean,
+    isOnEditMode: Boolean,
+    onButtonDeleteClick: () -> Unit,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = horizontalAlignment,
+    ) {
+        Button(
+            onClick = onButtonCreateClick,
+            enabled = isButtonEnabled,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(
+                text = if (isOnEditMode) {
+                    "Save profile"
+                } else {
+                    "Create new profile"
+                },
+            )
+        }
+        if (isOnEditMode) {
+            SimpleSpace(size = 20.dp)
+            Button(
+                onClick = onButtonDeleteClick,
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.error),
+                modifier = Modifier.fillMaxWidth(),
             ) {
-                Button(
-                    onClick = onButtonCreateClick,
-                    enabled = isButtonEnabled,
-                ) {
-                    Text(
-                        text = if (isOnEditMode) {
-                            "Save profile"
-                        } else {
-                            "Create new profile"
-                        },
-                    )
-                }
-                if (isOnEditMode) {
-                    SimpleSpace(size = 20.dp)
-                    Button(
-                        onClick = onButtonDeleteClick,
-                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.error),
-                    ) {
-                        Text(
-                            text = "Delete",
-                        )
-                    }
-                }
+                Text(
+                    text = "Delete",
+                )
             }
         }
     }
