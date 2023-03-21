1e1bcc6515a69538be2b749c6d68e1b5c9b0d59a
joasdavid <joasdavid@gmail.com>
2023-03-21 10:07:47 +0000

    #72 refactor DashboardHeader, the tablet version needs more work yet.

--- main/src/main/java/com/joasvpereira/main/compose/dashboard/DashBoardHeader.kt
+++ main/src/main/java/com/joasvpereira/main/compose/dashboard/DashBoardHeader.kt
@@ -5,15 +5,13 @@ import android.graphics.Bitmap
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -25,11 +23,16 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.core.graphics.drawable.toBitmap
+import com.joasvpereira.lib.compose.spacer.HorizontalSpace
 import com.joasvpereira.main.presentation.icons.DivisionIcons
+import pt.joasvpereira.coreui.preview.FoldablePreview
+import pt.joasvpereira.coreui.preview.LargePreview
+import pt.joasvpereira.coreui.preview.PreviewWrapperWithTheme
 import pt.joasvpereira.coreui.preview.UiModePreview
 import pt.joasvpereira.coreui.session.SessionIconHolder
-import pt.joasvpereira.coreui.theme.DynamicTheme
+import pt.joasvpereira.coreui.util.WindowSizeHelper
 import pt.joasvpereira.main.R
 
 @Composable
@@ -37,72 +40,131 @@ internal fun DashboardHeader(
     sessionName: String,
     sessionImage: Bitmap?,
     onSettingClicked: () -> Unit,
+    isExpanded: Boolean = WindowSizeHelper.currentWidthSize() == WindowSizeHelper.WidthSize.Expanded,
 ) {
-    Column(modifier = Modifier.fillMaxWidth()) {
-        RowTitleAndSettings(onSettingClicked)
+    ConstraintLayout(modifier = Modifier.fillMaxWidth()) {
+        val (
+            titleRef,
+            sessionRef,
+            settingsRef,
+        ) = createRefs()
 
-        Spacer(modifier = Modifier.height(28.5.dp))
-        RowUserInfo(sessionName, sessionImage)
-    }
-}
-
-@Composable
-private fun RowUserInfo(sessionName: String, sessionImage: Bitmap?) {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        Column(modifier = Modifier.weight(1f)) {
-            Text(
-                text = stringResource(R.string.dashboard_welcome),
-                style = MaterialTheme.typography.headlineSmall,
-            )
-            Text(
-                text = sessionName,
-                style = MaterialTheme.typography.labelLarge,
-            )
-        }
-        Box(
-            Modifier
-                .align(Alignment.Bottom)
-                .size(40.dp)
-                .background(MaterialTheme.colorScheme.primaryContainer, shape = CircleShape),
-        ) {
-            SessionIconHolder(
-                sessionName = sessionName,
-                sessionImage = sessionImage,
-            )
-        }
-    }
-}
-
-@Composable
-private fun RowTitleAndSettings(onSettingClicked: () -> Unit) {
-    Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-        Text(
-            text = "XOrganizer",
-            style = MaterialTheme.typography.headlineLarge,
-            modifier = Modifier.weight(1f),
-        )
-        Box(
+        AppTitle(
             modifier = Modifier
-                .clip(CircleShape)
-                .clickable { onSettingClicked() },
-        ) {
-            Icon(
-                modifier = Modifier
-                    .align(Alignment.Center)
-                    .size(24.dp),
-                imageVector = Icons.Default.Settings,
-                contentDescription = stringResource(R.string.settings_content_description),
-            )
-        }
+                .constrainAs(titleRef) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                },
+        )
+
+        ProfileIndicator(
+            sessionName = sessionName,
+            sessionImage = sessionImage,
+            isExpanded = isExpanded,
+            modifier = Modifier.constrainAs(sessionRef) {
+                end.linkTo(parent.end)
+            },
+        )
+
+        SettingsButton(
+            modifier = Modifier
+                .constrainAs(settingsRef) {
+                    top.linkTo(sessionRef.bottom, 20.dp)
+                    end.linkTo(sessionRef.end)
+                    start.linkTo(sessionRef.start)
+                },
+            isExpanded,
+            onSettingClicked,
+        )
     }
+}
+
+@Composable
+fun SettingsButton(modifier: Modifier, isExpanded: Boolean, onSettingClicked: () -> Unit) {
+    if (isExpanded) {
+        Button(
+            modifier = modifier,
+            onClick = onSettingClicked,
+        ) {
+            Row {
+                Text(text = "Settings")
+                HorizontalSpace(width = 10.dp)
+                Icon(
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .size(24.dp),
+                    imageVector = Icons.Default.Settings,
+                    contentDescription = stringResource(R.string.settings_content_description),
+                )
+            }
+        }
+    } else {
+        Icon(
+            modifier = modifier
+                .clip(CircleShape)
+                .size(24.dp)
+                .clickable { onSettingClicked() },
+            imageVector = Icons.Default.Settings,
+            contentDescription = stringResource(R.string.settings_content_description),
+        )
+    }
+}
+
+@Composable
+fun ProfileIndicator(sessionName: String, sessionImage: Bitmap?, isExpanded: Boolean, modifier: Modifier = Modifier) {
+    if (isExpanded) {
+        ProfileIndicatorExpanded(modifier = modifier, sessionName = sessionName, sessionImage = sessionImage)
+    } else {
+        ProfileLetterOrImageIndicator(modifier = modifier, sessionName = sessionName, sessionImage = sessionImage)
+    }
+}
+
+@Composable
+fun ProfileIndicatorExpanded(sessionName: String, sessionImage: Bitmap?, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = sessionName,
+            style = MaterialTheme.typography.labelLarge,
+        )
+        HorizontalSpace(width = 5.dp)
+        ProfileLetterOrImageIndicator(sessionName = sessionName, sessionImage = sessionImage)
+    }
+}
+
+@Composable
+private fun ProfileLetterOrImageIndicator(sessionName: String, sessionImage: Bitmap?, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .background(MaterialTheme.colorScheme.primaryContainer, shape = CircleShape),
+        contentAlignment = Alignment.Center,
+    ) {
+        SessionIconHolder(
+            sessionName = sessionName,
+            sessionImage = sessionImage,
+        )
+    }
+}
+
+@Composable
+fun AppTitle(modifier: Modifier) {
+    Text(
+        modifier = modifier,
+        text = "XOrganizer",
+        style = MaterialTheme.typography.headlineLarge,
+    )
 }
 
 @SuppressLint("UseCompatLoadingForDrawables")
 @UiModePreview
+@FoldablePreview
+@LargePreview
 @Composable
 private fun DashboardHeaderPreview() {
     val d = LocalContext.current.getDrawable(DivisionIcons.cactus.resId)?.toBitmap()
-    DynamicTheme {
+    PreviewWrapperWithTheme {
         Surface {
             DashboardHeader(
                 sessionName = "Joås V. Pereira",
